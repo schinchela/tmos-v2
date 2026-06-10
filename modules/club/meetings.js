6b8be0d8e0903b2493a20964e2b79c5f3f28cfc7
schinchela <suketh.chinchela@gmail.com>
2026-06-11 00:54:40 +0530

    Create meetings.js

---
+++ modules/club/meetings.js
@@ -0,0 +1,11 @@
+export function renderClubMeetings() {
+  return `
+    <section class="hero">
+      <p class="eyebrow">Meetings</p>
+      <h3>Meetings</h3>
+      <p>Meeting scheduling and agenda management.</p>
+    </section>
+  `;
+}
+
+export async function initClubMeetings() {}
