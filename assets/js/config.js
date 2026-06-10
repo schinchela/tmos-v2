0cdc48c889f9e92b4312999fac6e2dd1e5d124b3
schinchela <suketh.chinchela@gmail.com>
2026-06-11 01:21:07 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   APP_NAME: "TMOS Enterprise",
   APP_VERSION: "v2",
-  API_BASE_URL: "https://tmos-v2-api.suketh-chinchela.workers.dev",
+  API_BASE_URL: "https://api.tmos.rowepal.com",
   FRONTEND_URL: "https://tmos.rowepal.com",
   ENVIRONMENT: "development"
 };
